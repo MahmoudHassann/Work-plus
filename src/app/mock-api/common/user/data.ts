2503cb9d3b0013afb1be3444ed5a5cc99e9b4c54
mahmoud hassan <mahmoud.m.azab@gmail.com>
2023-02-03 13:50:34 +0200

    commit

--- src/app/mock-api/common/user/data.ts
+++ src/app/mock-api/common/user/data.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export const user = {
     id    : '2bfa2be5-7688-48d5-b5ac-dc0d9ac97f14',
-    name  : 'Mahmoud Mohamed',
-    email : 'mahmoud.m.azab@gmail.com',
-    avatar: 'assets/images/avatars/me.jpg',
+    name  : 'Admin',
+    email : 'admin@admin.com',
+    avatar: 'assets/images/avatars/profile avatar 3d.png',
     status: 'online'
 };
 
